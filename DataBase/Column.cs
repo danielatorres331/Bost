4cc24e96e9526adf646561f91858c1de966dd915
Daniela Torres Estrada <danielatorres331@hotmail.com>
2021-02-11 13:21:56 +0100

    Methods

--- DataBase/Column.cs
+++ DataBase/Column.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace DataBase
 {
-    class Column
+    public class Column
     {
+        public Column(string name)
+        {
+
+        }
+
+        public String GetValue(String value)
+        {
+            return null;
+        }
+
+        public List<String> GetValues(String value)
+        {
+            return null;
+        }
+
+        public void UpdateColumn()
+        {
+
+        }
+
+        public Column GetName()
+        {
+            return this;
+        }
+
+        public Column GetColumn()
+        {
+            return this;
+        }
+
+        public void DeleteValue()
+        {
+
+        }
+
     }
 }
