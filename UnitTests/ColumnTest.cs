e8f147be2aa2af1befc803a753394b8c85ff04fd
Laura <leguiluz005@ikasle.ehu.es>
2021-02-18 12:37:41 +0100

    Referece

--- UnitTests/ColumnTest.cs
+++ UnitTests/ColumnTest.cs
@@ -1,29 +1,33 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using DataBase;
+
 
 namespace UnitTests
 {
     [TestClass]
     public class ColumnTest
     {
+        Column c = new Column("cName");
+
         [TestMethod]
         public void TestColumn()
         {
+            Assert.IsNotNull(c1);
+            
         }
 
         [TestMethod]
         public void TestGetValue()
         {
-        }
-
-        [TestMethod]
-        public void TestGetValues()
-        {
+            
         }
 
         [TestMethod]
         public void TestGetName()
         {
+            Assert.IsNotNull(c.getName());
+            Assert.AreEqual("cName", c.getName());
         }
 
         [TestMethod]
