62e99721ecc54622fbeda410c19d1bdc240766d5
Amaia <Amaia@LAPTOP-6MA4G4HM>
2021-02-18 13:04:41 +0100

    First implementation in TableTest

--- UnitTests/TableTest.cs
+++ UnitTests/TableTest.cs
@@ -9,22 +9,33 @@ namespace UnitTests
         [TestMethod]
         public void TestTable()
         {
+            Table table1 = new Table("Table1");
+            Assert.IsNotNull(table1);
+
         }
         [TestMethod]
         public void TestAddColumn()
         {
+            Table table2 = new Table("Table2");
+            Column column1 = new Column("Column1");
+            table2.AddColumn(column1);
+            Assert.Equals(column1, table2.SearchColumnByName("Column1"));
+
         }
         [TestMethod]
         public void TestDeleteColumn()
         {
+            Table table3 = new Table("Table3");
+            Column column2 = new Column("Column2");
+            table2.AddColumn(column2);
+            table2.DeleteColumn(column2);
+            Assert.IsNull(table2.SearchColumnByName("Column2"));
         }
-        [TestMethod]
-        public void TestUpdateColumn()
-        {
-        }
+        
         [TestMethod]
         public void TestGetColumn()
         {
+
         }
         [TestMethod]
         public void TestAddRow()
