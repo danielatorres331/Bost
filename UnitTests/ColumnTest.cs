574e4bfc10416ede7c11c1f683f76955c5e3c634
Laura <leguiluz005@ikasle.ehu.es>
2021-02-22 12:42:57 +0100

    First ColumnTest

--- UnitTests/ColumnTest.cs
+++ UnitTests/ColumnTest.cs
@@ -1,59 +1,78 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using DataBase;
-
+using System.Collections.Generic;
 
 namespace UnitTests
 {
     [TestClass]
     public class ColumnTest
     {
-        Column c = new Column("cName");
+        Column ValuesList = new Column("cName");
+
+        public ColumnTest(String name)
+        { 
+            ValuesList.AddValue("value1");
+            ValuesList.AddValue("value2");
+            ValuesList.AddValue("value3");
+        }
 
         [TestMethod]
         public void TestColumn()
         {
-            Assert.IsNotNull(c1);
-            
+            Assert.IsNotNull(ValuesList);
         }
 
         [TestMethod]
         public void TestGetValue()
         {
-            
+            string value1 = ValuesList.GetValue(1);
+            string value2 =ValuesList.GetValue(2);
+            Assert.AreEqual(value1, "value1");
+            Assert.AreEqual(value2, "value2");
         }
-        */
 
         [TestMethod]
         public void TestGetName()
         {
-            Assert.IsNotNull(c.getName());
-            Assert.AreEqual("cName", c.getName());
-        }
-
-        [TestMethod]
-        public void TestGetColumn()
-        {
+            Assert.IsNotNull(ValuesList.GetName());
+            Assert.AreEqual("cName", ValuesList.GetName());
         }
 
         [TestMethod]
         public void TestDeleteValue()
         {
+            ValuesList.DeleteValue(3);
+            Column c1 = new Column("column1");
+            c1.AddValue("value1");
+            c1.AddValue("value2");
+            Assert.AreEqual(ValuesList, c1);
+           
         }
 
         [TestMethod]
         public void TestSetValue()
         {
+            ValuesList.SetValue(2, "newValueName");
+            Column c2 = new Column("column1");
+            c2.AddValue("value1");
+            c2.AddValue("newValueName");
+            c2.AddValue("value3");
+            Assert.AreEqual(c2, ValuesList);
+           // Assert.AreEqual("newValueName", ValuesList.GetValue(2));
         }
 
         [TestMethod]
         public void TestAddValue()
         {
+            ValuesList.AddValue("newValue");
+            Assert.AreEqual("newValue", ValuesList.GetValue(4));
         }
 
         [TestMethod]
         public void TestGetIndex()
         {
+            Assert.AreEqual(2, ValuesList.GetIndex("value2"));
         }
     }
 }
