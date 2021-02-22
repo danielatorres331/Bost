ff388279cc01f71d23eedd39bbf836112fa56f37
Mariia Zubryk <mariia.zubryk@maristak.net>
2021-02-22 12:43:03 +0100

    Update namespace and DataBaseTest

--- UnitTests/DataBaseTest.cs
+++ UnitTests/DataBaseTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using BostDB;
 
 namespace UnitTests
 {
@@ -9,14 +10,14 @@ namespace UnitTests
         [TestMethod]
         public void TestDataBase()
         {        
-            DataBase db1 = new DataBase("DataBase1",Bost,contraseña);        
+            DataBase db1 = new DataBase("DataBase1","Bost","contraseña");        
             Assert.IsNotNull(db1);
         }
 
         [TestMethod]
         public void TestDropTable()
         {
-            DataBase db2 = new DataBase("DataBase2", Bost, contraseña);
+            DataBase db2 = new DataBase("DataBase2", "Bost", "contraseña");
             Table table1 = new Table("Table1");
             db2.DropTable(table1);
             Assert.IsNull(table1);
@@ -26,21 +27,22 @@ namespace UnitTests
         [TestMethod]
         public void TestAddTable()
         {
-            DataBase db3 = new DataBase("DataBase3", Bost, contraseña);
+            DataBase db3 = new DataBase("DataBase3", "Bost", "contraseña");
             Table table2 = new Table("Table2");
             db3.AddTable(table2);
-            Assert.Equals(table2 , db.getTableByName("Table2"));
+            Assert.Equals(table2 , db3.SearchTableByName("Table2"));
 
         }
 
-        /*
+        
         [TestMethod]
         public void TestSeachTableByName()
         {
-            DataBase db4 = new DataBase("DataBase4", Bost, contraseña);
+            DataBase db4 = new DataBase("DataBase4", "Bost", "contraseña");
             Table table3 = new Table("Table3");
             db4.AddTable(table3);
-            Assert.Equals(table3, db.getTableByName("Table3"));
+            Assert.Equals(table3, db4.SearchTableByName("Table3"));
 
         }
     }
+}
