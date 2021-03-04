6e0b410f0181dc8814e2c9be0bc6f75d6b1bf684
Mariia Zubryk <mariia.zubryk@maristak.net>
2021-03-04 13:11:40 +0100

    MiniSQL

---
+++ DataBase/MiniSqlParser/SelectAll.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.MiniSqlParser
+{
+    public class SelectAll : IQuery
+    {
+        private string m_table;
+       
+        
+
+        public  SelectAll(string table)
+        {
+            m_table = table;
+            
+        }
+        public string Run(DataBase database)
+        {
+          //  return database.SelectAll(m_table).ToString;
+          return null;
+        }
+    }
+}
