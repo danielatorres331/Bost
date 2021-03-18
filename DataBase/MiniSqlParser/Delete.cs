6f925155118f46ea76b6943c0957095992e15106
Amaia <Amaia@LAPTOP-6MA4G4HM>
2021-03-18 12:51:34 +0100

    #15  deletion pattern and delete class for the delete query

---
+++ DataBase/MiniSqlParser/Delete.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.MiniSqlParser
+{
+    class Delete : IQuery
+    {
+        private string m_table;
+        private string m_column;
+
+        public string Table()
+        {
+            return m_table;
+        }
+        public string Column() 
+        {
+            return m_column;
+        }
+        public Delete(string table, string column)
+        {
+            m_table = table;
+            m_column = column;
+
+        }
+        public string Run(DataBase database)
+        {
+            return null;
+        }
+    }
+}
