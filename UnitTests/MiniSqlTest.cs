a8d9c9b34565a427e6e5f83c7a3666e3bbfa30fc
Daniela Torres Estrada <dtorres012@ikasle.ehu.eus>
2021-03-22 13:26:52 +0100

    #16 MiniSqlTest updated, but they don't passed it

--- UnitTests/MiniSqlTest.cs
+++ UnitTests/MiniSqlTest.cs
@@ -11,27 +11,33 @@ namespace UnitTests
         public void Parsing()
         {
             IQuery query = Parser.Parse("SELECT * FROM Table1;");
-            Assert.IsTrue(query is SelectAll);
+            
             Assert.AreEqual("Table1", (query as SelectAll).Table());
         }
 
+        [TestMethod]
         public void TestInsert()
         {
-            IQuery query = Parser.Parse("INSERT INTO Table1 VALUES (34567)");
-            Assert.IsTrue(query is Insert);
-            Assert.AreEqual("Table1","34567", (query as Insert).Column());
-        }
-
-        public void TestDelete()
-        { 
+            IQuery query = Parser.Parse("INSERT INTO Table1 VALUES (34567);");
             
+            Assert.AreEqual("Table1","(34567)", (query as Insert).Table());
         }
 
+       /* [TestMethod]
+       public void TestDelete()
+        {
+           IQuery query = Parser.Parse("DELETE FROM Table1 WHERE VALUES = '34567' ");
+           Assert.IsTrue(query is Delete);
+           Assert.AreEqual("Table1", "34567", (query as Delete).Table());
+           
+        }
+
+        [TestMethod]
         public void TestUpdate()
         { 
         
         
-        }
+        }*/
 
     }
 }
