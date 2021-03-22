4cbf7534df9fafd6cffc9bc819f52dae7a8947d1
Daniela Torres Estrada <dtorres012@ikasle.ehu.eus>
2021-03-22 12:32:29 +0100

    #16 MiniSqlTest Methods

--- UnitTests/MiniSqlTest.cs
+++ UnitTests/MiniSqlTest.cs
@@ -17,9 +17,9 @@ namespace UnitTests
 
         public void TestInsert()
         {
-            IQuery query = Parser.Parse("INSERT INTO ");
-            Assert.IsTrue(query is SelectAll);
-            Assert.AreEqual("Table1", (query as SelectAll).Table());
+            IQuery query = Parser.Parse("INSERT INTO Table1 VALUES (34567)");
+            Assert.IsTrue(query is Insert);
+            Assert.AreEqual("Table1","34567", (query as Insert).Column());
         }
 
         public void TestDelete()
