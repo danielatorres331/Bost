d47f2de09a259a496ed17600b9510cf167a1eba2
Amaia <Amaia@LAPTOP-6MA4G4HM>
2021-03-22 13:00:55 +0100

    #15 delete pattern completed in Delete class and in Parser class.

--- DataBase/MiniSqlParser/Delete.cs
+++ DataBase/MiniSqlParser/Delete.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BostDB.MiniSqlParser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace DataBase.MiniSqlParser
     {
         private string m_table;
         private string m_column;
+        private string m_operador;
+        private string m_value;
 
         public string Table()
         {
@@ -19,13 +22,16 @@ namespace DataBase.MiniSqlParser
         {
             return m_column;
         }
-        public Delete(string table, string column)
+        public Delete(string table, string column, string operador, string value)
         {
             m_table = table;
             m_column = column;
+            m_operador = operador ;
+            m_value = value;
+
 
         }
-        public string Run(DataBase database)
+        public string Run(BostDB.DataBase database)
         {
             return null;
         }
--- DataBase/MiniSqlParser/Parser.cs
+++ DataBase/MiniSqlParser/Parser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataBase.MiniSqlParser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ namespace BostDB.MiniSqlParser
         {
            const string selectAllPattern = @"SELECT \* FROM([a-zA-Z0-9]+)";
            const string selectColumnsPattern = @"SELECT ([a-zA-Z0-9,]+)FROM([a-zA-Z0-9]+)";
-            const string delete = @"DELETE FROM ([a-zA-Z0-9.]+) WHERE ([a-zA-Z0-9.]+) .{1,3} ([a-zA-Z0-9.]+);";
+            const string deletePattern = @"DELETE FROM ([a-zA-Z0-9.]+) WHERE ([a-zA-Z0-9.]+) .{1,2} ([a-zA-Z0-9.]+);";
            const string insertPattern = @"INSERT INTO ([a-zA-Z0-9]+) VALUES \(([^\)]+)\);";
 
 
@@ -33,6 +34,12 @@ namespace BostDB.MiniSqlParser
                 //Gets a collection of groups matched by the regular expression
                 SelectColumns selectColumns = new SelectColumns(match.Groups[2].Value, columnNames);
                 return selectColumns;
+            }
+            match= Regex.Match(miniSqlSentence, deletePattern);
+            if (match.Success)
+            {
+                Delete deleteValue = new Delete(match.Groups[1].Value, match.Groups[2].Value,match.Groups[3].Value, match.Groups[4].Value);
+                return deleteValue;
             }
 
             return null;
