31a62742935b4dcf8857c3b4844af3993c577868
Laura <leguiluz005@ikasle.ehu.es>
2021-04-15 13:00:11 +0200

    #19 Add the stopwatch and finished the console program successfully

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using BostDB;
 using BostDB.MiniSqlParser;
 
@@ -8,6 +9,10 @@ namespace Console
     {
         static void Main(string[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            double time=0.00;
+
             int index = 1;
             //Console.WriteLine("Hello World!");
             string[] lines = File.ReadAllLines("Input.txt");
@@ -21,20 +26,29 @@ namespace Console
                 {
                     IQuery query = Parser.Parse(line);
                     if (query != null)
-                        System.Console.WriteLine(query.Run(dataBase));
+                    {
+                        System.Console.WriteLine(query.Run(dataBase) + "    (" + stopwatch.Elapsed.TotalSeconds.ToString("0.00 s") + ")");
+                        time += stopwatch.Elapsed.TotalSeconds;
+                    }
                     else
+                    {
                         System.Console.WriteLine(Messages.WrongSyntax);
+                    }
 
                 }
                 else 
                 {
+                    System.Console.WriteLine("TOTAL TIME: " + time.ToString("0.00 s"));
                     index++;
                     dataBase = new DataBase("DataBase" + index.ToString(), "Bost", "contraseña");
                     System.Console.WriteLine("");
                     System.Console.WriteLine("# TEST " + index.ToString());
+                    time = 0.00;
                 }
 
             }
+            System.Console.WriteLine("TOTAL TIME: " + time.ToString("0.00 s"));
+            stopwatch.Stop();
         }
     }
 }
