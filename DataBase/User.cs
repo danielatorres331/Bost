ae64b688ce6fe7d09d5636ac956e0a550c14d56f
Mariia Zubryk <mariia.zubryk@maristak.net>
2021-04-19 13:27:48 +0200

    #20 Do Class User and Method SaveUsers in the DataBase

---
+++ DataBase/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BostDB
+{
+    public class User
+    {
+        private string _user;
+        private string _password;
+        private Profile _profile;
+
+        public User(string us, string pass, Profile prof)
+        {
+            us = _user;
+            pass = _password;
+            prof = _profile;
+        }
+
+        public string GetUser()
+        {
+            return _user;
+        }
+
+        public String Save()
+        {
+            return _user +"," + _password +"," +_profile;
+        }
+    }
+}
